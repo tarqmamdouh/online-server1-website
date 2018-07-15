ba86eeba1145e4a058adaea296f335fc376a6b34
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-15 04:04:35 +0200

    Added username to signup and navbar when logged in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :username , presence: true , uniqueness: {case_sensitive:false},
+            length: {minimum: 3 , maximum: 25}
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
 end
