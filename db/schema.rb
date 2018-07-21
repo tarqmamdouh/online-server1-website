c06756c2915bf7786899ba3ec75f3632d4e5238f
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-22 01:53:38 +0200

    added validates to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_21_232445) do
+ActiveRecord::Schema.define(version: 2018_07_21_234542) do
 
-  create_table "news", force: :cascade do |t|
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
