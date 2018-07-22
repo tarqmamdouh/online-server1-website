6400018740fd08df5444b5713ba9f795f7488b86
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-22 02:30:44 +0200

    create articles from ui

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'confirmed' , to: 'welcome#confirmed'
   get 'unconfirmed', to: 'welcome#unconfirmed'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
