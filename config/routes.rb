06880d005b70026fa381912a6e3e358a708d27ed
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-24 18:57:47 +0200

    Created Categories

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get 'confirmed' , to: 'welcome#confirmed'
   get 'unconfirmed', to: 'welcome#unconfirmed'
   resources :articles
+  resources :categories ,  except: [:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
