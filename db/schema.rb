13aa54560ee99ddb460cd365c9279cc742b7e6fd
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-24 20:29:25 +0200

    Categories done working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_24_161034) do
+ActiveRecord::Schema.define(version: 2018_07_24_174234) do
+
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
