7428853fe0bdb085d6f7fefdc6d8ebc0ee9834a7
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-25 22:27:43 +0200

    Comments Donegit add .

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :article_categories
   has_many :categories, through: :article_categories
-has_many :comments
+has_many :comments, dependent: :destroy
   validates :title , presence: true , length: {minimum: 3 , maximum: 50}
   validates :description , presence: true, length: {minimum: 10 , maximum: 400}
   validates :user_id , presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :articles
-  has_many :comments
+  has_many :comments, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
