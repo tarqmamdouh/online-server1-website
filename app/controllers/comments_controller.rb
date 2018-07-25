18bad962ff45417c4d85c0ac0bc0bcd77d0bd2c8
Mostafa Abdelsalam <mostafa.abdelsalam96@gmail.com>
2018-07-25 22:00:29 +0200

    CRUD comments with comment creator username

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :find_article
+  before_action :find_comment , only: [:destroy, :edit , :update]
   def create
 @comment = @article.comments.create(params[:comment].permit(:content))
     @comment.user_id = current_user.id
@@ -12,11 +13,28 @@ class CommentsController < ApplicationController
     end
 
   end
+  def destroy
+@comment.destroy
+    redirect_to article_path(@article)
+  end
+  def edit
+
+  end
+  def update
+    if @comment.update(params[:comment].permit(:content))
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 
 
   private
   def find_article
     @article = Article.find(params[:article_id])
 
+  end
+  def find_comment
+    @comment = @article.comments.find(params[:id])
   end
 end
