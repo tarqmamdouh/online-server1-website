7f93702e46e2d18f3a1fa9ed6ea4668392af3daa
TarqMamdouh <tarq.mamdouh1996@gmail.com>
2018-08-08 06:23:51 +0200

    fixed Styles

--- app/controllers/user/confirmations_controller.rb
+++ app/controllers/user/confirmations_controller.rb
@@ -6,7 +6,7 @@ class User::ConfirmationsController < Devise::ConfirmationsController
       signed_in_root_path(resource)
     else
       #new_session_path(resource_name)
-      @DB = SQL.connect_account
+      @DB = SQL.connect_atmaccount
 
       #prepare sql paramaters
 
