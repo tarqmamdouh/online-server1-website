dda7ea4930e1047de48a6ade4049ed2c6bbe9811
TarqMamdouh <tarq.mamdouh1996@gmail.com>
2018-08-24 10:14:43 +0200

    removed test group

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
-ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
-  html_tag.html_safe
-end
 # Load the Rails application.
 require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
