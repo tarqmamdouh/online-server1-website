f365b751b7822c02abf2ba5b1807e94f02a3afb3
TarqMamdouh <tarq.mamdouh1996@gmail.com>
2018-08-29 18:25:10 +0200

    fixed attachinary table

--- db/migrate/20180817051328_create_tcomments.rb
+++ db/migrate/20180817051328_create_tcomments.rb
@@ -1,6 +1,7 @@
 class CreateTcomments < ActiveRecord::Migration[5.2]
   def change
     create_table :tcomments do |t|
+
       t.text :content
       t.references :ticket, foreign_key: true
       t.references :user, foreign_key: true
